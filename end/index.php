514a351ce8f775fc745c15f9dd1dbbfb0424ccfb
geekstartup42 <985343961@qq.com>
2019-02-26 19:29:55 +0800

    Update index.php

--- end/index.php
+++ end/index.php
@@ -2,6 +2,11 @@
 php  后端服务实现。
 
 1、实现 环状 消息提醒 队列。
+这个队列  类似 日历  ，确实很像  
+参考如下  
+https://www.cnblogs.com/jcli/p/calendar_recur_rule.html
+https://tools.ietf.org/html/rfc2445#section-4.3.10
+
 
 2、数据库交互。
 
